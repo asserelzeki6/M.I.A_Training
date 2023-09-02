a6c54016141a480bb0739eb9c0ef42e05959e7be
asserelzeki6 <asserelzeki@gmail.com>
2023-09-02 04:56:25 +0300

    First task concerning cpp tasks

---
+++ Task 1/Counting Down/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+// counting down
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+        cout << t+1 << endl;
+    cout << "Blast off to the moon!" << endl;
+    return 0;
+}
